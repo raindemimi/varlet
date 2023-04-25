8eaaee4f266819e51c2481e040c7d84da16ce717
yuanyu <2271173295@qq.com>
2023-04-25 12:54:01 +0800

    修改meige忽略文件

---
+++ packages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>新建的文件</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+}
+
+</script>
+<style scoped lang='scss'>
+</style>
